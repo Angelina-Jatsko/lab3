9ad69de042022f614267ff42c68c519f61a1ec79
Angelina-Jatsko <134391685+Angelina-Jatsko@users.noreply.github.com>
2024-04-05 10:13:28 +0300

    Реализация GridIcon

--- app/src/main/java/com/raywenderlich/android/lab1/screens/GridScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/GridScreen.kt
@@ -1,11 +1,16 @@
 package com.raywenderlich.android.lab1.screens
 
 import android.content.Intent.ShortcutIconResource
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Icon
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Build
 import androidx.compose.material.icons.filled.Check
@@ -15,7 +20,12 @@ import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.ThumbUp
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.raywenderlich.android.lab1.R
 import com.raywenderlich.android.lab1.router.BackButtonHandler
 import com.raywenderlich.android.lab1.router.FundamentalsRouter
 import com.raywenderlich.android.lab1.router.Screen
@@ -34,13 +44,37 @@ private val items = listOf(
     Icons.Filled.ThumbUp
 )
 
+@ExperimentalFoundationApi
 @Composable
 fun GridScreen() {
-    GridView(columCount = 3)
+  LazyVerticalGrid(
+      modifier = Modifier.fillMaxSize(),
+      columns = GridCells.Fixed(3) ,
+      content = {
+          items(count = items.size) {item ->
+             GridIcon(IconResource(items[item], true))
+          }
+      }
+  )
 
     BackButtonHandler {
         FundamentalsRouter.navigateTo(Screen.Navigation)
     }
+}
+
+@Composable
+fun GridIcon(iconResource: IconResource) {
+    val color = if (iconResource.isVisible)
+        colorResource(R.color.colorPrimary)
+    else Color.Transparent
+
+    Icon(
+        imageVector = iconResource.imageVector,
+        tint = color,
+        contentDescription = stringResource(R.string.grid_icon),
+        modifier = Modifier
+            .size(80.dp, 80.dp)
+    )
 }
 
 @Composable
@@ -79,6 +113,16 @@ fun RowItem(rowItems: List<IconResource>) {
 
 @Composable
 fun RowScope.GridIcon(iconResource: IconResource) {
-
+  val color = if (iconResource.isVisible)
+      colorResource(R.color.colorPrimary)
+  else Color.Transparent
+  Icon(
+      imageVector = iconResource.imageVector,
+      tint = color,
+      contentDescription = stringResource(R.string.grid_icon),
+      modifier = Modifier
+          .size(80.dp, 80.dp)
+          .weight(1f)
+      )
 }
 data class IconResource(val imageVector: ImageVector, val isVisible: Boolean)
